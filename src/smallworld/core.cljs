f798f12c1dba63314dd297202c5585bc274fb0e5
Devon Zuegel <devonzuegel@gmail.com>
2021-12-26 12:43:17 -0300

    progress on frontend + css

--- src/smallworld/core.cljs
+++ src/smallworld/core.cljs
@@ -2,23 +2,38 @@
   (:require [reagent.core :as r]
             [goog.dom]))
 
-(def friends (atom ()))
+(defonce friends (r/atom ()))
+(defonce count- (r/atom 1))
 
 (def by-id goog.dom.getElement)
 
 (defn app-container []
-  [:div.container
-   [:h1 {}
-    "hello small world!!!!!!!!"]
-   [:p "foo bar"]
-   [:pre @friends]])
+  [:div
+   [:div.nav
+    [:div.logo
+     [:div.logo-globe]
+     [:div.logo-text "small world"]]
+    [:div.links
+     [:a "about"]
+     [:span.links-spacer "·"]
+     [:a "log out " [:b "@username"]]]]
+
+   [:div.container
+    [:p "foo bar"]
+    [:hr]
+    [:pre "@count:"]
+    [:pre @count-]
+    [:p.links-spacer]
+    [:pre "@friends!!!!!!!"]
+    [:pre (pr-str @friends)]]])
 
 (r/render-component [app-container] (by-id "app"))
 
-(-> (.fetch js/window "http://localhost:3001/friends")
+(-> (.fetch js/window "/friends")
     (.then #(.json %))
     (.then (fn [result]
-             (js/console.log result)
              (swap! friends conj result)
-             (js/console.log "friends: " friends))))
+             (swap! count- inc)
+             (js/console.log "friends: ")
+             (js/console.log result))))
 
