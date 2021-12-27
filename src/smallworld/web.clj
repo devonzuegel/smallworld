77ae366c41dfac39cf6a980e2984bcb99018f426
Devon Zuegel <devonzuegel@gmail.com>
2021-12-27 14:23:58 -0300

    refactor haversine fn

--- src/smallworld/web.clj
+++ src/smallworld/web.clj
@@ -87,17 +87,16 @@
 ; c = 2 ⋅ atan2( √a, √(1−a) )
 ; d = R ⋅ c
 ; where φ is latitude, λ is longitude, R is earth’s radius (mean radius = 6,371km);
-(defn haversine
-  "Implementation of Haversine formula. Takes two sets of latitude/longitude pairs and returns the shortest great circle distance between them (in km)"
-  [{lon1 :lng lat1 :lat} {lon2 :lng lat2 :lat}]
-  (when (some nil? [lon1 lat1 lon2 lat2])
-    (throw (Exception. "coordinates {:lat, :lng} must not be nil")))
-  (let [R 6378.137 ; Radius of Earth in km
+; "Implementation of Haversine formula. Takes two sets of latitude/longitude pairs and returns the shortest great circle distance between them (in km)"
+(defn haversine [{lon1 :lng lat1 :lat} {lon2 :lng lat2 :lat}]
+  (assert (every? some? [lon1 lat1 lon2 lat2]) "coordinates {:lat, :lng} must not be nil")
+  (let [R 6378.137 ; radius of Earth in km
         dlat (Math/toRadians (- lat2 lat1))
         dlon (Math/toRadians (- lon2 lon1))
         lat1 (Math/toRadians lat1)
         lat2 (Math/toRadians lat2)
-        a (+ (* (Math/sin (/ dlat 2)) (Math/sin (/ dlat 2))) (* (Math/sin (/ dlon 2)) (Math/sin (/ dlon 2)) (Math/cos lat1) (Math/cos lat2)))]
+        a (+ (* (Math/sin (/ dlat 2)) (Math/sin (/ dlat 2)))
+             (* (Math/sin (/ dlon 2)) (Math/sin (/ dlon 2)) (Math/cos lat1) (Math/cos lat2)))]
     (* R 2 (Math/asin (Math/sqrt a)))))
 
 (def stored-coordinates {:sf         {:lat 37.78007888793945,   :lng -122.42015838623047}
