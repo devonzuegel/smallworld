79e5ec6fb2ad6cf2883914a08bcd0470ddc994d3
Devon Zuegel <devonzuegel@gmail.com>
2022-01-18 12:37:21 -0500

    add comments to bin/deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,4 +2,7 @@ echo ""
 echo "running command:"
 echo "    heroku deploy:jar target/smallworld.jar --app small-world-friends"
 echo ""
+echo "note: this will deploy the *existing* jar to the heroku app."
+echo "      run bin/make-jar.sh if you want to create a new jar."
+echo ""
 heroku deploy:jar target/smallworld.jar --app small-world-friends
