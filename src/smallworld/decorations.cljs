ac245f2c800eff886725e36824a1d038454c7a0b
Devon Zuegel <devonzuegel@gmail.com>
2022-02-14 12:57:19 -0500

    fix plane animation

--- src/smallworld/decorations.cljs
+++ src/smallworld/decorations.cljs
@@ -1,24 +1,47 @@
 (ns smallworld.decorations
   (:require [smallworld.mapbox]
-            [clojure.pprint :as pp]
             [cljsjs.mapbox]
             [goog.dom]
+            [reagent.core :as r]
             [goog.dom.classlist :as gc]))
 
-(defn animated-globe []
-  (let [handle-hover (fn [] (let [elem (goog.dom/getElement "logo-animation")
-                                  start-animation #(gc/remove elem "no-animation")
-                                  stop-animation #(gc/add elem "no-animation")
-                                  stop-after-iteration #((.addEventListener elem "webkitAnimationIteration" stop-animation) ;; for Chrome
-                                                         (.addEventListener elem "animationiteration" stop-animation) ;; for Firefox
-                                                         (.addEventListener elem "MSAnimationIteration" stop-animation) ;; for IE
-                                                         (.addEventListener elem "animationiteration" stop-animation))]
-                              (.addEventListener elem "mouseover" start-animation)
-                              (.addEventListener elem "mouseout" stop-after-iteration)))]
-    (js/setTimeout handle-hover 1000) ; give time to load the animation
-    [:div {:class "globe-loader fas fa-globe-americas"} [:i.fas.fa-plane]]))
+(def debug? false)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; simple spinner ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn simple-loading-animation []
   [:svg.loader
    [:path {:fill "#fff"
            :d "M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"}]])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; plane/globe animation ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defonce plane-animation-iterations (r/atom 0))
+
+(defn start-animation []
+  (when debug? (println "start-animation"))
+  (let [elem (goog.dom/getElement "logo-animation")]
+    (gc/remove elem "no-animation")
+    (reset! plane-animation-iterations 0)))
+
+(defn stop-animation []
+  (when debug? (println "stop-animation:" @plane-animation-iterations))
+  (swap! plane-animation-iterations inc)
+  (when (>= @plane-animation-iterations 3)
+    (let [elem (goog.dom/getElement "logo-animation")]
+      (gc/add elem "no-animation"))))
+
+(defn animated-globe []
+  (js/setTimeout #(let [elem (goog.dom/getElement "logo-animation")]
+                    (.addEventListener elem "mouseover" start-animation)
+                    (.addEventListener elem "webkitAnimationIteration" stop-animation) ;; for Chrome
+                    (.addEventListener elem "animationiteration" stop-animation) ;; for Firefox
+                    (.addEventListener elem "MSAnimationIteration" stop-animation) ;; for IE
+                    (.addEventListener elem "animationiteration" stop-animation)
+                    (when debug? (println "adding listeners to elem")))
+                 1000) ; give time to load the animation
+  [:div {:class "globe-loader fas fa-globe-americas"} [:i.fas.fa-plane]])
