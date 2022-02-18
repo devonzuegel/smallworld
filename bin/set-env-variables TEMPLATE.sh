2e3496dd4f17aa2891d1c59daa2f4f474e0496fe
Devon Zuegel <devonzuegel@gmail.com>
2022-02-18 10:21:55 -0500

    add LEIN_JVM_OPTS to env vars to suppress OpenJDK 64-Bit Server VM warning

--- bin/set-env-variables TEMPLATE.sh
+++ bin/set-env-variables TEMPLATE.sh
@@ -5,3 +5,4 @@ export TWITTER_ACCESS_TOKEN_SECRET=get from https://developer.twitter.com
 export BING_MAPS_API_KEY=get from Bing Developer
 export COOKIE_STORE_SECRET_KEY=generate a 16-char random string
 export DATABASE_URL=set up in Heroku
+export LEIN_JVM_OPTS="-XX:TieredStopAtLevel=1" # suppresses OpenJDK 64-Bit Server VM warning: https://stackoverflow.com/a/67695691/2639250
