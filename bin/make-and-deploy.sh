1a2adbcfd83a724f248e74c27dbe50fd98eaef7a
Devon Zuegel <devonzuegel@gmail.com>
2022-02-18 21:19:36 -0500

    add comment to bin/make-and-deploy.sh

--- bin/make-and-deploy.sh
+++ bin/make-and-deploy.sh
@@ -1,5 +1,5 @@
 bin/make-jar.sh &&
-git add target/smallworld.jar -f &&
+git add target/smallworld.jar -f &&  # the rest of the /target file is in .gitignore
 git commit -m "built target/smallworld.jar" &&
 bin/deploy.sh &&
 bin/heroku-logs.sh
