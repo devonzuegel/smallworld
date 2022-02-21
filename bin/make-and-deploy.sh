7cee0ddd3f8eab245e6b1643c838aa9308f60881
Devon Zuegel <devonzuegel@gmail.com>
2022-02-21 00:28:23 -0500

    fix deploy scripts

--- bin/make-and-deploy.sh
+++ bin/make-and-deploy.sh
@@ -4,7 +4,7 @@ echo "" &&
 echo "running command:" &&
 echo "    source bin/set-env-variables.sh" &&
 git add target/smallworld.jar -f &&  # the rest of the /target file is in .gitignore
-git commit -m "built target/smallworld.jar" &&
+git commit -m "build target/smallworld.jar" &&
 
 bin/deploy.sh &&
 bin/heroku-logs.sh
