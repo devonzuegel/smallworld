devonzuegel/smallworld

commit ef704bdb6d525033a411aad7f4739bd460493d0f

depend on :locations in more places

src/smallworld/user_data.clj
@@ -63,20 +63,21 @@
      :locations [(when (not (str/blank? friend-main-location)) {:special-status "twitter-location" ; formerly called "main location"
                                                                 :name friend-main-location
                                                                 :coords friend-main-coords
-                                                                :distances {:to-main (coordinates/distance-btwn current-name-coords friend-main-coords)
-                                                                            :to-name (coordinates/distance-btwn current-main-coords friend-main-coords)}})
+                                                                :distances {(:name (second (:locations current-user))) (coordinates/distance-btwn current-name-coords friend-main-coords)
+                                                                            (:name (first  (:locations current-user))) (coordinates/distance-btwn current-main-coords friend-main-coords)}})
                  (when (not (str/blank? friend-name-location)) {:special-status "from-display-name" ; formerly called "name location"
                                                                 :name friend-name-location
                                                                 :coords friend-name-coords
-                                                                :distances {:to-main (coordinates/distance-btwn current-name-coords friend-name-coords)
-                                                                            :to-name (coordinates/distance-btwn current-main-coords friend-name-coords)}})]
-     ; TODO: rm everything below this line
-     :distance (when (not is-current-user?)
-                 {:name-main (coordinates/distance-btwn current-name-coords friend-main-coords)
-                  :name-name (coordinates/distance-btwn current-name-coords friend-name-coords)
-                  :main-main (coordinates/distance-btwn current-main-coords friend-main-coords)
-                  :main-name (coordinates/distance-btwn current-main-coords friend-name-coords)})
-     :main-location friend-main-location
-     :name-location friend-name-location
-     :main-coords   friend-main-coords
-     :name-coords   friend-name-coords}))
+                                                                :distances {(:name (second (:locations current-user))) (coordinates/distance-btwn current-name-coords friend-name-coords)
+                                                                            (:name (first  (:locations current-user))) (coordinates/distance-btwn current-main-coords friend-name-coords)}})]
+    ;;  ; TODO: rm everything below this line
+    ;;  :distance (when (not is-current-user?)
+    ;;              {:name-main (coordinates/distance-btwn current-name-coords friend-main-coords)
+    ;;               :name-name (coordinates/distance-btwn current-name-coords friend-name-coords)
+    ;;               :main-main (coordinates/distance-btwn current-main-coords friend-main-coords)
+    ;;               :main-name (coordinates/distance-btwn current-main-coords friend-name-coords)})
+    ;;  :main-location friend-main-location
+    ;;  :name-location friend-name-location
+    ;;  :main-coords   friend-main-coords
+    ;;  :name-coords   friend-name-coords
+     }))

src/smallworld/web.clj
@@ -177,8 +177,8 @@
   (m/my-memoize --fetch-abridged-friends abridged-friends-cache))
 
 (defn get-users-friends [req & [screen-name]]
-  (let [main-location (or (:main_location_corrected (get-settings req)) "")
-        name-location (or (:name_location_corrected (get-settings req)) "")
+  (let [main-location (or (:main_location_corrected (get-settings req)) "") ; TODO: instead of saving these to settings individually, save them as a list of :locations
+        name-location (or (:name_location_corrected (get-settings req)) "") ; TODO: instead of saving these to settings individually, save them as a list of :locations
         corrected-curr-user (merge ; TODO: if main- or name-coords have been updated, remove them from the list and add the updated ones
                              (get-current-user req)
                              {:main-location main-location

src/smallworld/mapbox.cljs
@@ -196,7 +196,7 @@
                                                         :classname (when (= (:screen-name friend)
                                                                             (:screen-name curr-user))
                                                                      "current-user")
-                                                        :location       (:main-location friend)
+                                                        :location       (:name location)
                                                         :img-url        (:profile_image_url_large friend)
                                                         :user-name      (:name friend)
                                                         :screen-name    (:screen-name friend)

src/smallworld/screens/home.cljs
@@ -5,7 +5,8 @@
             [smallworld.mapbox      :as mapbox]
             [smallworld.user-data   :as user-data]
             [smallworld.util        :as util]
-            [smallworld.session     :as session]))
+            [smallworld.session     :as session]
+            [clojure.string :as str]))
 
 (def     *debug?    (r/atom false))
 (defonce *locations (r/atom :loading))
@@ -52,8 +53,9 @@
 (defn screen []
   [:<>
    (nav)
-   (let [main-location (or (:main_location_corrected @settings/*settings) (:main-location @session/*store))
-         name-location (or (:name_location_corrected @settings/*settings) (:name-location @session/*store))]
+   (let [locations     (:locations @session/*store)
+         main-location (or (:main_location_corrected @settings/*settings) (first (filter #(= (:special-status %) "twitter-location")  locations)))
+         name-location (or (:name_location_corrected @settings/*settings) (first (filter #(= (:special-status %) "from-display-name") locations)))]
      [:<>
       [:div.home-page
        #_[:div.current-user [user-data/render-user nil @session/*store]] ; TODO: cleanup
@@ -74,39 +76,63 @@
            [:br]
            [:p "if you don't want to update your Twitter settings, you can still explore the map below"]])
 
-        (when-not (empty? main-location)
-          [:div.category
-           [:div.friends-list.header
-            [:div.left-side.mapbox-container {:style {:width "90px"}}
-             [minimap "main-location-map" main-location]
-             (when-not (clojure.string/blank? main-location) [:div.center-point])]
-            [:div.right-side
-             [:div.based-on "based on your location, you live in:"]
-             [:input {:type "text"
-                      :value main-location
-                      :autoComplete "off"
-                      :auto-complete "off"
-                      :on-change #(print "TODO:")}]
-             [:div.small-info-text "this won't update your Twitter profile :)"]]]
-           (user-data/render-friends-list :main-main "based near" main-location)
-           (user-data/render-friends-list :main-name "visiting"   main-location)])
+        (doall (map-indexed (fn [i location-data]
+                              (let [minimap-id (str "minimap-location-" i)]
+                                [:div.category {:key i}
+                                 [:div.friends-list.header
+                                  [:div.left-side.mapbox-container {:style {:width "90px"}}
+                                   [minimap minimap-id (:name location-data)]
+                                   (when-not (str/blank? (:name location-data)) [:div.center-point])]
+                                  [:div.right-side
+                                   [:div.based-on (condp = (:special-status location-data)
+                                                    "twitter-location"  "based on your location, you live in:"
+                                                    "from-display-name" "based on your name, you live in:"
+                                                    "another location you're tracking...")]
+                                   [:input {:type "text"
+                                            :value (:name location-data)
+                                            :autoComplete "off"
+                                            :auto-complete "off"
+                                            :on-change #(print "TODO:")}]
+                                   [:div.small-info-text "this won't update your Twitter profile :)"]]]
+                                 (user-data/render-friends-list i "twitter-location"  "based near" (:name location-data))
+                                 (user-data/render-friends-list i "from-display-name" "visiting"   (:name location-data))]))
+                            (remove nil? locations)))
+        [:br] [:br]
+        ;; [:hr]
+        ;; [:br] [:br]
+        ;; (when-not (empty? main-location)
+        ;;   [:div.category
+        ;;    [:div.friends-list.header
+        ;;     [:div.left-side.mapbox-container {:style {:width "90px"}}
+        ;;      [minimap "main-location-map" main-location]
+        ;;      (when-not (clojure.string/blank? main-location) [:div.center-point])]
+        ;;     [:div.right-side
+        ;;      [:div.based-on "based on your location, you live in:"]
+        ;;      [:input {:type "text"
+        ;;               :value main-location
+        ;;               :autoComplete "off"
+        ;;               :auto-complete "off"
+        ;;               :on-change #(print "TODO:")}]
+        ;;      [:div.small-info-text "this won't update your Twitter profile :)"]]]
+        ;;    (user-data/render-friends-list :main-main "based near" main-location)
+        ;;    (user-data/render-friends-list :main-name "visiting"   main-location)])
 
-        (when-not (empty? name-location)
-          [:div.category
-           [:div.friends-list.header
-            [:div.left-side.mapbox-container {:style {:width "90px"}}
-             [minimap "name-location-map" name-location]
-             (when-not (clojure.string/blank? name-location) [:div.center-point])]
-            [:div.right-side
-             [:div.based-on "based on your name, you live in:"]
-             [:input {:type "text"
-                      :value name-location
-                      :autoComplete "off"
-                      :auto-complete "off"
-                      :on-change #(print "TODO:")}]
-             [:div.small-info-text "this won't update your Twitter profile :)"]]]
-           (user-data/render-friends-list :name-main "based near" name-location)
-           (user-data/render-friends-list :name-name "visiting"   name-location)])
+        ;; (when-not (empty? name-location)
+        ;;   [:div.category
+        ;;    [:div.friends-list.header
+        ;;     [:div.left-side.mapbox-container {:style {:width "90px"}}
+        ;;      [minimap "name-location-map" name-location]
+        ;;      (when-not (clojure.string/blank? name-location) [:div.center-point])]
+        ;;     [:div.right-side
+        ;;      [:div.based-on "based on your name, you live in:"]
+        ;;      [:input {:type "text"
+        ;;               :value name-location
+        ;;               :autoComplete "off"
+        ;;               :auto-complete "off"
+        ;;               :on-change #(print "TODO:")}]
+        ;;      [:div.small-info-text "this won't update your Twitter profile :)"]]]
+        ;;    (user-data/render-friends-list :name-main "based near" name-location)
+        ;;    (user-data/render-friends-list :name-name "visiting"   name-location)])
 
         [:br]
         [:div.track-new-location-field (decorations/plus-icon) "track another location"]

src/smallworld/user_data.cljs
@@ -37,23 +37,47 @@
             :target "_blank"}
         [:span.location (:name first-location)]]]]]))
 
-(defn get-close-friends [distance-key max-distance]
+; TODO: the logic in this needs some serious cleanup; probably requires refactoring the data model too
+(defn get-close-friends [curr-user-location-name friend-location-key max-distance]
   (->> @*friends
-       (sort-by #(get-in % [:distance distance-key]))
-       (filter (closer-than max-distance distance-key))))
+       (filter (fn [friend] ; not all friends will have both LOCATION and DISPLAY NAME LOCATION set, so filter those out
+                 (let [friend-locations (:locations friend)
+                       has-location? (-> #(= (:special-status %) friend-location-key)
+                                         (filter friend-locations)
+                                         first
+                                         nil?
+                                         not)]
+                   has-location?)))
 
-(def *collapsed? (r/atom {:main-main false
-                          :main-name false
-                          :name-main false
-                          :name-name false}))
+       (filter (fn [friend] (let [friend-locations (:locations friend)
+                                  friend-location  (-> #(= (:special-status %) friend-location-key)
+                                                       (filter friend-locations)
+                                                       first)
+                                  distance-to-curr-user-location (get-in friend-location
+                                                                         [:distances (keyword curr-user-location-name)])]
+                              (and (not (nil? distance-to-curr-user-location))
+                                   (> max-distance distance-to-curr-user-location)))))
 
-(defn render-friends-list [friends-list-key verb-gerund location-name]
-  (let [friends-list      (if (= :loading @*friends)
-                            []
-                            (get-close-friends friends-list-key 100))
+       (sort-by (fn [friend] (let [friend-locations (:locations friend)
+                                   friend-location  (-> #(= (:special-status %) friend-location-key)
+                                                        (filter friend-locations)
+                                                        first)
+                                   distance-to-curr-user-location (get-in friend-location
+                                                                          [:distances (keyword curr-user-location-name)])]
+                               distance-to-curr-user-location)))))
+
+(def *expanded? (r/atom {}))
+
+(defn render-friends-list [curr-user-location-i friend-location-key verb-gerund curr-user-location-name]
+  (assert (or (= friend-location-key "twitter-location")
+              (= friend-location-key "from-display-name"))) ; TODO: add Scheme to encode this more nicely
+  (let [key-pair        [curr-user-location-i friend-location-key]
+        friends-list    (if (= :loading @*friends)
+                          []
+                          (get-close-friends curr-user-location-name friend-location-key 100))
         list-count        (count friends-list)
         friend-pluralized (if (= list-count 1) "friend is" "friends are")
-        collapsed?        (get @*collapsed? friends-list-key)]
+        expanded?        (boolean (get @*expanded? key-pair))]
 
     [util/error-boundary
      [:div.friends-list
@@ -66,18 +90,18 @@
           [:<>
            [:p.location-info
             {:on-click ; toggle collapsed state
-             #(swap! *collapsed? assoc friends-list-key (not collapsed?))}
-            (decorations/triangle-icon (clojure.string/join " "  ["caret" (if collapsed? "right" "down")]))
+             #(swap! *expanded? assoc key-pair (not expanded?))}
+            (decorations/triangle-icon (clojure.string/join " "  ["caret" (if expanded? "right" "down")]))
             [:<>
              list-count " "
              friend-pluralized " "
-             verb-gerund " " location-name ":"]]
-           (when-not collapsed?
+             verb-gerund " " curr-user-location-name ":"]]
+           (when-not expanded?
              [:div.friends (map-indexed render-user friends-list)])]
 
           [:div.no-friends-found
            (decorations/x-icon)
-           "0 friends are " verb-gerund " " location-name]))]]))
+           "0 friends are " verb-gerund " " curr-user-location-name]))]]))
 
 (defn refresh-friends []
   (util/fetch "/friends/refresh"

commit 1d651a9e3ba426550126f6adb351d7e7114d4393

fixed the bug introduced in ef704bd

src/smallworld/user_data.clj
@@ -63,14 +63,15 @@
      :locations [(when (not (str/blank? friend-main-location)) {:special-status "twitter-location" ; formerly called "main location"
                                                                 :name friend-main-location
                                                                 :coords friend-main-coords
-                                                                :distances {(:name (second (:locations current-user))) (coordinates/distance-btwn current-name-coords friend-main-coords)
-                                                                            (:name (first  (:locations current-user))) (coordinates/distance-btwn current-main-coords friend-main-coords)}})
+                                                                :distances (when (not is-current-user?) ; distances aren't relevant if the friend whose data we're abridging is the current user
+                                                                             {(:name (second (:locations current-user))) (coordinates/distance-btwn current-name-coords friend-main-coords)
+                                                                              (:name (first  (:locations current-user))) (coordinates/distance-btwn current-main-coords friend-main-coords)})})
                  (when (not (str/blank? friend-name-location)) {:special-status "from-display-name" ; formerly called "name location"
                                                                 :name friend-name-location
                                                                 :coords friend-name-coords
-                                                                :distances {(:name (second (:locations current-user))) (coordinates/distance-btwn current-name-coords friend-name-coords)
-                                                                            (:name (first  (:locations current-user))) (coordinates/distance-btwn current-main-coords friend-name-coords)}})]
-    ;;  ; TODO: rm everything below this line
+                                                                :distances (when (not is-current-user?) ; distances aren't relevant if the friend whose data we're abridging is the current user
+                                                                             {(:name (second (:locations current-user))) (coordinates/distance-btwn current-name-coords friend-name-coords)
+                                                                              (:name (first  (:locations current-user))) (coordinates/distance-btwn current-main-coords friend-name-coords)})})]    ;;  ; TODO: rm everything below this line
     ;;  :distance (when (not is-current-user?)
     ;;              {:name-main (coordinates/distance-btwn current-name-coords friend-main-coords)
     ;;               :name-name (coordinates/distance-btwn current-name-coords friend-name-coords)

src/smallworld/web.clj
@@ -60,7 +60,7 @@
              oauth-verifier (get-in req [:params :oauth_verifier])
              access-token   (oauth/oauth-access-token (util/get-env-var "TWITTER_CONSUMER_KEY") oauth-token oauth-verifier)
              api-response   (fetch-current-user--with-access-token access-token)
-             current-user   (user-data/abridged api-response (get-current-user req))
+             current-user   (user-data/abridged api-response {:screen-name (:screen-name api-response)})
              screen-name    (:screen-name api-response)]
          (when debug?
            (pp/pprint "twitter verify_credentials.json:")