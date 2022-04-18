cfb0497eca75e10df79685dfd7636cf4c2557f30
Devon Zuegel <devonzuegel@gmail.com>
2022-04-18 10:53:35 -0400

    add refresh-friends-from-twitter to worker.clj

--- src/smallworld/worker.clj
+++ src/smallworld/worker.clj
@@ -1,15 +1,22 @@
 (ns smallworld.worker
   (:gen-class) ; forces Clojure to make a Java class that you can find through the jar
-  (:require [smallworld.db :as db]))
+  (:require [smallworld.db :as db]
+            [smallworld.web :as backend]))
+
+(defn timestamp [] (str (new java.util.Date)))
+
+(defn log [string]
+  (println (timestamp) "--" string))
 
 (defn -main []
   (println)
-  (println "-----------------------------------")
-  (println "starting worker.clj:" (str (new java.util.Date)))
-  (println "-----------------------------------"))
-
-(comment
-  (db/show-all db/settings-table)
-  (db/show-all db/friends-table)
-  ;
-  )
+  (println "===============================================")
+  (log "starting worker.clj")
+  (println)
+  (let [all-users (db/select-all db/settings-table)]
+    (log (str "preparing to refresh friends for " (count all-users) " users"))
+    (map backend/refresh-friends-from-twitter all-users))
+  (println)
+  (log "finished worker.clj")
+  (println "===============================================")
+  (println))
