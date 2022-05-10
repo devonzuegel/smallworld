f9f334826a7348dc4b6f851a2be3e345f02d9062
Devon Zuegel <devonzuegel@gmail.com>
2022-05-10 11:45:51 -0400

    fix worker.clj

--- src/smallworld/worker.clj
+++ src/smallworld/worker.clj
@@ -10,8 +10,6 @@
 
 (defn try-to-refresh-friends [total-count]
   (fn [i user]
-    ; TODO: figure out why none of the stuff inside of here is printing when
-    ; it's run by the bash script, even though it is run when run from the repl
     (try
       (util/log (str "[user " i "/" total-count "] refresh friends for " (:screen_name user)))
       (backend/refresh-friends-from-twitter user)
@@ -31,14 +29,16 @@
         n-failures (count @failures)
         curried-refresh-friends (try-to-refresh-friends n-users)]
     (util/log (str "preparing to refresh friends for " n-users " users"))
-    (map-indexed curried-refresh-friends all-users)
-    (util/log (str "finished refreshing friends for " n-failures " users: " n-failures " failures"))
-    (email/send {:to "avery.sara.james@gmail.com"
-                 :subject (str "[" (util/get-env-var "ENVIRONMENT") "] worker.clj finished: " n-failures " failures out of " n-users " users")
-                 :type "text/plain"
-                 :body (str "finished refreshing friends for " n-failures " users: " n-failures " failures"
-                            "\n\n"
-                            "users that failed:\n" (with-out-str (pp/pprint @failures)))}))
+    (println)
+    (doall (map-indexed curried-refresh-friends all-users))
+    (util/log (str "finished refreshing friends for " n-users " users: " n-failures " failures"))
+    (println)
+    (email/send-email {:to "avery.sara.james@gmail.com"
+                       :subject (str "[" (util/get-env-var "ENVIRONMENT") "] worker.clj finished: " n-failures " failures out of " n-users " users")
+                       :type "text/plain"
+                       :body (str "finished refreshing friends for " n-failures " users: " n-failures " failures"
+                                  "\n\n"
+                                  "users that failed:\n" (with-out-str (pp/pprint @failures)))}))
   (println)
   (util/log "finished worker.clj")
   (println "===============================================")
