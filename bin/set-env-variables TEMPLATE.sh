9c370e211d5f842ae0a16e3e30e0700b270e6c98
Devon Zuegel <devonzuegel@gmail.com>
2022-06-04 12:00:53 -0400

    add JAVA_OPTS to env vars

--- bin/set-env-variables TEMPLATE.sh
+++ bin/set-env-variables TEMPLATE.sh
@@ -6,4 +6,5 @@ export BING_MAPS_API_KEY=get from Bing Developer
 export COOKIE_STORE_SECRET_KEY=generate a 16-char random string
 export DATABASE_URL=set up in Heroku
 export LEIN_JVM_OPTS="-XX:TieredStopAtLevel=1" # suppresses OpenJDK 64-Bit Server VM warning: https://stackoverflow.com/a/67695691/2639250
+export JAVA_OPTS="-Xmx300m -Xss512k -XX:CICompilerCount=2 -Dfile.encoding=UTF-8" # match the max heap/stack size set by Heroku
 export SENDGRID_API_KEY=get from https://sendgrid.com
