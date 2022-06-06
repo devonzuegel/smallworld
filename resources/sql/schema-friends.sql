5f97e407aefd00e8d2bdca02779c29360510153c
Devon Zuegel <devonzuegel@gmail.com>
2022-06-05 23:21:43 -0400

    auto-update updated_at for friends table

--- resources/sql/schema-friends.sql
+++ resources/sql/schema-friends.sql
@@ -8,7 +8,36 @@ create table if not exists friends (
 
 --- split here ---
 
--- add index on request_key
+--------------------------------------------------------------------------------
+---- auto-update updated_at ----------------------------------------------------
+--------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION public.set_current_timestamp_updated_at()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$function$;
+
+--- split here ---
+
+CREATE TRIGGER set_updated_at
+BEFORE UPDATE ON friends
+FOR EACH ROW
+EXECUTE FUNCTION set_current_timestamp_updated_at();
+
+--------------------------------------------------------------------------------
+---- add an index on request_key -----------------------------------------------
+--------------------------------------------------------------------------------
+
+--- split here ---
+
 CREATE INDEX index_request_key
 ON friends (request_key);
 
