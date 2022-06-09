8851305b5a59272805538fada0d72b4964fd813b
Devon Zuegel <devonzuegel@gmail.com>
2022-06-09 17:09:18 -0400

    increase time on bin/restart-heroku-continuously.sh

--- bin/restart-heroku-continuously.sh
+++ bin/restart-heroku-continuously.sh
@@ -1,11 +1,11 @@
 while true
 do
   now=$(date +"%T")
-  n_seconds=100
-  n_iterations=10
+  n_seconds=150
+  n_iterations=15
   echo "$now - sleeping for $n_seconds seconds before next restart"
-  for i in {1..10}; do
-    sleep 10 # seconds
+  for i in {1..15}; do
+    sleep 15 # seconds
     now=$(date +"%T")
     echo "$now - still sleeping ($i/$n_iterations)"
   done
