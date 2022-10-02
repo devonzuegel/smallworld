df4d50ec39539deffaf914a385378088596dec6a
Devon Zuegel <devonzuegel@gmail.com>
2022-10-02 07:46:46 -0400

    disable refetch schedule

--- src/smallworld/web.clj
+++ src/smallworld/web.clj
@@ -599,18 +599,17 @@
          (if (= (:cause (Throwable->map e)) "Scheduler already started")
            (println "scheduler already started") ; it's fine, this isn't a real error, so just continue
            (throw e))))
-  (println "starting scheduler to run every 10 minutes")
-  ;; (println "TEMPORARY: not starting scheduler for email-update-worker!")
-  )
+  ;; (println "starting scheduler to run every 10 minutes")
+  (println "TEMPORARY: not starting scheduler for email-update-worker!"))
 
 ; start the email-update worker that refreshes users' twitter info/friends
-(let [env (util/get-env-var "ENVIRONMENT")]
-  (if (= env (:prod util/ENVIRONMENTS))
-    (let [id (timely/start-schedule
-              (timely/scheduled-item (timely/every 10 :minutes) email-update-worker))]
-      (reset! email-update-worker-id id)
-      (println "\nstarted email update worker with id:" @email-update-worker-id))
-    (println "\nnot starting email update worker because ENVIRONMENT is" env "not" (:prod util/ENVIRONMENTS))))
+#_(let [env (util/get-env-var "ENVIRONMENT")]
+    (if (= env (:prod util/ENVIRONMENTS))
+      (let [id (timely/start-schedule
+                (timely/scheduled-item (timely/every 10 :minutes) email-update-worker))]
+        (reset! email-update-worker-id id)
+        (println "\nstarted email update worker with id:" @email-update-worker-id))
+      (println "\nnot starting email update worker because ENVIRONMENT is" env "not" (:prod util/ENVIRONMENTS))))
 
   ; start garbage collection worker
 (let [id (timely/start-schedule
