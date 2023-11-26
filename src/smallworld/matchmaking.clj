c33c872bab5cc0fe13d524a0bebddfbc7838dcd6
Devon Zuegel <devonzuegel@gmail.com>
2023-11-26 17:40:27 -0500

    finally figured out the keyword issue (was mixing up with symbol...)

--- src/smallworld/matchmaking.clj
+++ src/smallworld/matchmaking.clj
@@ -1,6 +1,8 @@
 (ns smallworld.matchmaking (:require [clojure.core.memoize :as memoize]
+                                     [clojure.pprint       :as pp]
                                      [smallworld.airtable  :as airtable]
-                                     [smallworld.util :as util]))
+                                     [smallworld.util      :as util]
+                                     #_[goog.object :as obj]))
 
 (def airtable-base {:api-key (util/get-env-var "AIRTABLE_BASE_API_KEY")
                     :base-id "appF2K8ThWvtrC6Hs"})
@@ -8,10 +10,13 @@
 (defn seconds [n] (* n 1000))
 (defn minutes [n] (* n 60 (seconds 1)))
 
-(defn fetch-all-bios []
+(defn fetch-all-bios! []
   (println "fetching bios from Airtable")
   (airtable/get-in-base airtable-base ["bios-devons-test"]))
 
 (def fetch-all-bios-memoized
-  (memoize/ttl fetch-all-bios {} :ttl/threshold (minutes 30)))
+  (memoize/ttl fetch-all-bios! {} :ttl/threshold (minutes 30)))
 
+(defn get-all-bios []
+  (let [all-bios-raw (fetch-all-bios-memoized)]
+    (map (fn [bio] (merge (:fields bio) {:id (:id bio)})) all-bios-raw)))
--- src/smallworld/screens/meetcute.cljs
+++ src/smallworld/screens/meetcute.cljs
@@ -8,8 +8,20 @@
   (util/fetch "/api/matchmaking/bios" (fn [result]
                                         (println "result:" result)
                                         (swap! bios (fn [_] result)))))
+
+(defn stringify-all-symbol-keys [obj]
+  (if (map? obj)
+    (into {} (map (fn [[k v]]
+                    #_(println "k:" k)
+                    #_(println "v:" v)
+                    #_(println "")
+                    [(str k) (stringify-all-symbol-keys v)]) obj))
+    obj))
+
 (defn get-field [field bio]
-  (get-in bio [:fields field]))
+  (get-in bio [field])
+  ;; (get-in bio [:fields field])
+  )
 
 (defn get-key-names [bio]
   (map first (get bio :fields)))
@@ -25,27 +37,33 @@
       (map-indexed
        (fn [i bio]
          [:div {:key i :style {:background "#ffffff11" :margin "16px" :border-radius "8px" :padding "16px"}}
+          [:pre "First name: " (get-field (keyword "First name") bio)]
+          [:pre " Last name: " (get-field (keyword "Last name") bio)]
+          [:pre "Created By: " (get-field (keyword "Created By") bio)]
+          [:pre "     Email: " (get-field (keyword "Email") bio)]
+          [:pre "     Phone: " (get-field (keyword "Phone") bio)]
+          [:pre "Who invited you to this matchmaking experiment?" (get-field (symbol "Who invited you to this matchmaking experiment?") bio)]
+          [:hr]
+          [:pre (str "bio:")]
+          [:pre (pr-str bio)]
+          [:pre "-------------------------------------------"]
           (map (fn [j] [:p {:style {:margin "16px"} :key (str i "-" j)}
                         (str j) [:br] [:br]
                         (name j) [:br] [:br]
                         (get-field (name j) bio)
                         (str (type j)) [:br] [:br] [:hr] [:br]])
                (get-key-names bio))
-          [:p
-          ;;  (get-field (symbol "First name") bio) "!!!!!!!"
-           #_(get-field (symbol "Last name") bio)]]
-         #_(let [fields (get bio :fields)]
-             [:div
-              [:hr]
-              [:h1 (get-field "First name" record)]
-              (map-indexed (fn [j [k v]]
-                             [:div {:key (str i "-" j)}
-                              [:b (str k)]
-                              (if (= k :Pictures)
-                                [:p (map-indexed (fn [k2 v2] [:img {:src (:url v2) :key k2 :style {:height "120px" :margin "8px"}}]) v)]
-                                [:p (str v)])
-                              [:br]])
-                           fields)]))
+          (let [fields (get bio :fields)]
+            [:div
+             [:hr]
+             (map-indexed (fn [j [k v]]
+                            [:div {:key (str i "-" j)}
+                             [:b (str k)]
+                             (if (= k :Pictures)
+                               [:p (map-indexed (fn [k2 v2] [:img {:src (:url v2) :key k2 :style {:height "120px" :margin "8px"}}]) v)]
+                               [:p (str v)])
+                             [:br]])
+                          fields)])])
        @bios)])])
 
 (defn screen []
