278b58a58d3579f03d8271643cc42e3b4cc4bfab
Devon Zuegel <devonzuegel@gmail.com>
2023-12-04 14:29:50 -0500

    meetcute: add separate tabs for Bios & Profile

--- src/smallworld/screens/meetcute.cljs
+++ src/smallworld/screens/meetcute.cljs
@@ -1,34 +1,28 @@
 (ns smallworld.screens.meetcute (:require [reagent.core    :as r]
+                                          [clojure.string :as str]
                                           [smallworld.util :as util]))
 
 (def debug? (r/atom false))
 (def bios   (r/atom nil))
+(def phone (r/atom nil))
+(def profile (r/atom nil))
+(def current-tab (r/atom :profile))
 
 (defn fetch-bios []
   (println "fetching bios")
   (util/fetch "/api/matchmaking/bios" (fn [result]
-                                        (println "result:" result)
                                         (swap! bios (fn [_] result)))))
-
-(defn stringify-all-symbol-keys [obj]
-  (if (map? obj)
-    (into {} (map (fn [[k v]]
-                    #_(println "k:" k)
-                    #_(println "v:" v)
-                    #_(println "")
-                    [(str k) (stringify-all-symbol-keys v)]) obj))
-    obj))
 
 (defn get-field [bio field]
   (get-in bio [(keyword field)]))
 
 (defn get-key-names [bio] (map first bio))
 
+(def btn-styles {:color "white" :border "3px solid #ffffff33" :padding "12px" :border-radius "8px" :cursor "pointer" :margin "6px"})
+
 (defn -screen []
   [:div {:style {:margin-left "auto" :margin-right "auto" :width "80%"}}
    [:h1 {:style {:font-size 80 :line-height "2em"}} (str "All bios " (when-not (nil? @bios) (str "(" (count @bios) ")")))]
-   [:button {:on-click #(reset! debug? (not @debug?)) :style {:color "white" :border "3px solid #ffffff33" :padding "12px" :border-radius "8px" :cursor "pointer"}}
-    (str "Debug: " @debug?)]
    [:br]
    [:br]
    (if (nil? @bios)
@@ -56,9 +50,48 @@
                                                                                (get-field bio "Pictures"))]])
        @bios)])])
 
+(def phone-input-error (r/atom nil))
+
+(defn valid-phone [phone]
+  ; strip the phone number of all non-numeric characters, then check if it's a valid phone number. if yes, return true; if not, return false:
+  (let [phone (or phone "")
+        phone (str/replace phone #"[^0-9]" "")]
+    (and (not (empty? phone)) (re-find #"^\d{10}$" phone))))
+
+
+(defn profile-tab []
+  [:div {:style {:margin-left "auto" :margin-right "auto" :width "80%"}}
+   [:h1 {:style {:font-size 80 :line-height "2em"}} "Your profile"]
+
+   [:div {:style {:color "red" :min-height "1.4em"}} @phone-input-error]
+   [:p "Your phone number: " [:input {:type "text"
+                                      :value @phone
+                                      :on-change #(reset! phone (-> % .-target .-value))
+                                      :style {:background "#ffffff22" :border-radius "8px" :padding "6px 8px" :margin-right :4px}}]
+    [:button {:style btn-styles
+              :on-click (fn [] (if (valid-phone @phone)
+                                 (println "TODO: find bio with associated phone number")
+                                 (reset! phone-input-error "Please enter a valid phone number")))} "Sign in / sign up"]]
+   ;
+   ])
+
+(defn nav-btns []
+  [:div {:style {:margin "12px"}}
+   [:button {:on-click #(reset! current-tab :home) :style btn-styles} "All bios"]
+   [:button {:on-click #(reset! current-tab :profile) :style btn-styles} "Your profile"]
+   [:button {:on-click #(reset! debug? (not @debug?)) :style (merge btn-styles {:float "right"})} (str "Debug: " @debug?)]
+   [:br]])
+
 (defn screen []
   (r/create-class
    {:component-did-mount (fn []
                            (println "component-did-mount")
                            (fetch-bios))
-    :reagent-render (fn [] [-screen])}))
+    :reagent-render (fn []
+                      [:div
+                       [nav-btns]
+
+                       (case @current-tab
+                         :profile (profile-tab)
+                         :home (-screen)
+                         (-screen))])}))
