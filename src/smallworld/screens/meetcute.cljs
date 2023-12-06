00140dda74adc4224fdeda1aaf314589007d73a6
Devon Zuegel <devonzuegel@gmail.com>
2023-12-06 17:59:54 -0500

    progress!

--- src/smallworld/screens/meetcute.cljs
+++ src/smallworld/screens/meetcute.cljs
@@ -30,11 +30,16 @@
                                 "Home base city"
                                 "I'm interested in..."
                                 "What makes this person awesome?"
+                                "Pictures"
                                 "Gender"])
 
+(defn format-phone [phone]
+  (let [digits-only (str/replace phone #"[^0-9]" "")]
+    (str "(" (subs digits-only 0 3) ") " (subs digits-only 3 6) "-" (subs digits-only 6 10))))
+
 (defn bio-row [i [key-name value]]
-  [:tr {:key i :style {:padding "24px" :background "#ffffff11"}}
-   [:td {:style {:padding "8px" :text-align "right" :font-size ".85em" :opacity ".75"}} key-name]
+  [:tr {:key i :style {:padding "24px" :background "#ffffff11" :vertical-align "top"}}
+   [:td {:style {:padding "8px" :text-align "right" :font-size ".85em" :opacity ".75" :max-width "200px"}} key-name]
    [:td {:style {:padding "8px" :text-align "left"}} value]])
 
 (defn render-bio [i bio]
@@ -44,14 +49,14 @@
                      ["Email"                            (get-field bio "Email")]
                      ["First name"                       (get-field bio "First name")]
                      ["Last name"                        (get-field bio "Last name")]
-                     ["Phone"                            (get-field bio "Phone")]
+                     ["Phone"                            (format-phone (get-field bio "Phone"))]
                      ["Home base city"                   (get-field bio "Home base city")]
                      ["I'm interested in..."             (get-field bio "I'm interested in...")]
                      ["What makes this person awesome?"  (get-field bio "What makes this person awesome?")]
                      ["Gender"                           (get-field bio "Gender")]
                      ["Pictures"                         (map-indexed (fn [k2 v2] [:img {:src (:url v2) :key k2 :style {:height "180px" :margin "8px 8px 0 0"}}])
                                                                       (get-field bio "Pictures"))]]]
-     [:table {:style {:margin-top "12px" :border-radius "8px" :padding "6px" :vertical-align "top" :line-height "1.2em"}}
+     [:table {:style {:margin-top "12px" :border-radius "8px" :padding "6px" :line-height "1.2em"}}
       [:tbody
        (map-indexed bio-row key-values)]])])
 
@@ -106,12 +111,54 @@
   [:input {:type "text"
            :value (get-field @profile field-name)
            :on-change (change-profile-field field-name)
-           :style {:background "#ffffff22" :border-radius "8px" :padding "6px 8px" :margin-right :4px}}])
+           :style {:background "#ffffff22" :border-radius "8px" :padding "6px 8px" :margin-right :4px :width "95%"}}])
 
-(defn editable-text-input [field-name]
+(defn editable-textbox [field-name]
   [:textarea {:value (get-field @profile field-name)
               :on-change (change-profile-field field-name)
-              :style {:background "#ffffff22" :border-radius "8px" :padding "6px 8px" :margin-right :4px}}])
+              :style {:background "#ffffff22"
+                      :border-radius "8px"
+                      :padding "6px 8px"
+                      :margin-right "12px"
+                      :min-height "150px"
+                      :color "#d9d3cc"
+                      :font-size ".9em"
+                      :width "95%"}}])
+
+(defn in? [list str] (some #(= str %) list))
+
+(defn change-interested-in-checkbox [-e]
+  (let [men-checkbox (js/document.getElementById "men-checkbox")
+        women-checkbox (js/document.getElementById "women-checkbox")]
+    (js/console.log "  men: " (.-checked men-checkbox))
+    (js/console.log "women: " (.-checked women-checkbox))))
+
+(def checkbox-values (r/atom ["A"]))
+
+(defn handle-checkbox-change [value]
+  (fn [event]
+    (let [checked? (.-checked (.-target event))]
+      (swap! checkbox-values
+             (if checked?
+               #(conj % value)
+               #(remove (fn [v] (= value v)) %))))))
+
+(defn checkbox [value]
+  [:div
+   [:input {:type "checkbox"
+            :value value
+            :checked (in? @checkbox-values value)
+            :style {:margin "12px"}
+            :on-change (handle-checkbox-change value)}]
+   [:label {:for (str value "-checkbox")} value] ; TODO: make this a component that takes a value and returns the checkbox and label
+   ])
+
+(defn checkboxes-component []
+  (let [values ["A" "B" "C"]]
+    (fn []
+      [:div
+       (for [value values] ^{:key value} [checkbox value])
+       [:div "Selected Values: " (str @checkbox-values)]])))
 
 (defn profile-tab []
   [:div {:style {:margin-left "auto" :margin-right "auto" :width "80%"}}
@@ -129,20 +176,44 @@
    (when (exists? @profile)
      [:div
       [:button {:style btn-styles :on-click update-profile!} "Save changes"]
+      [:pre "@profile:" (pr-str (select-keys @profile [;(keyword "Anything else you'd like your potential matches to know?")
+                                                       ;(keyword "Social media links")
+                                                       ;(keyword "Email")
+                                                       ;(keyword "First name")
+                                                       ;(keyword "Last name")
+                                                       ;(keyword "Phone")
+                                                       (keyword "Home base city")
+                                                       (keyword "I'm interested in...")
+                                                       ;(keyword "What makes this person awesome?")
+                                                       (keyword "Gender")]))]
       [:div {:style {:margin "16px 0 24px 0"}}
-       (let [key-values [["Anything else you'd like your potential matches to know?" (editable-input "Anything else you'd like your potential matches to know?")]
-                         ["Social media links"               (editable-input "Social media links")]
+       (let [key-values [["Phone"                            (format-phone (get-field @profile "Phone"))] ; do not make this editable!
+                         ["Anything else you'd like your potential matches to know?" (editable-textbox "Anything else you'd like your potential matches to know?")]
+                         ["Social media links"               (editable-textbox "Social media links")]
                          ["Email"                            (editable-input "Email")]
                          ["First name"                       (editable-input "First name")]
                          ["Last name"                        (editable-input "Last name")]
-                         ["Phone"                            (get-field @profile "Phone")] ; do not make this editable!
                          ["Home base city"                   (editable-input "Home base city")]
-                         ["I'm interested in..."             (editable-input "I'm interested in...")]
-                         ["What makes this person awesome?"  (editable-input "What makes this person awesome?")]
+                         ["I'm interested in..."
+                          (let [interested-in-list (get-field @profile "I'm interested in...")]
+                            (println "fields: " interested-in-list)
+                            (println "women?: " (in? interested-in-list "Women"))
+                            ; checkboxes to select Men or Women, and when they change, update the @profile:
+                            [:div
+                             [:pre "current value:" (pr-str (get-field @profile "I'm interested in..."))]
+                             [:div {:style {:margin-right "12px"}}
+                              [:input {:type "checkbox" :id "men-checkbox"   :checked (in? interested-in-list "Men")   :on-change change-interested-in-checkbox}]
+                              [:label {:for "men-checkbox"} "Men"]]
+                             [:div {:style {:margin-right "12px"}}
+                              [:input {:type "checkbox" :id "women-checkbox" :checked (in? interested-in-list "Women") :on-change change-interested-in-checkbox}]
+                              [:label {:for "women-checkbox"} "Women"]]
+                            ;
+                             ])]
+                         ["What makes this person awesome?"  (editable-textbox "What makes this person awesome?")]
                          ["Gender"                           (editable-input "Gender")]
                          ["Pictures"                         (map-indexed (fn [k2 v2] [:img {:src (:url v2) :key k2 :style {:height "180px" :margin "8px 8px 0 0"}}])
                                                                           (get-field @profile "Pictures"))]]]
-         [:table {:style {:margin-top "12px" :border-radius "8px" :padding "6px" :vertical-align "top" :line-height "1.2em"}}
+         [:table {:style {:margin-top "12px" :border-radius "8px" :padding "6px" :vertical-align "top" :line-height "1.2em" :width "100%"}}
           [:tbody
            (map-indexed bio-row key-values)]])]])])
 
@@ -154,15 +225,14 @@
    [:br]])
 
 (defn screen []
-  (r/create-class
-   {:component-did-mount (fn []
-                           (println "component-did-mount")
-                           (fetch-bios))
-    :reagent-render (fn []
-                      [:div
-                       [nav-btns]
+  (checkboxes-component)
+  #_(r/create-class
+     {:component-did-mount (fn [] (fetch-bios))
+      :reagent-render (fn []
+                        [:div
+                         [nav-btns]
 
-                       (case @current-tab
-                         :profile (profile-tab)
-                         :home (home-tab)
-                         (home-tab))])}))
+                         (case @current-tab
+                           :profile (profile-tab)
+                           :home (home-tab)
+                           (home-tab))])}))
