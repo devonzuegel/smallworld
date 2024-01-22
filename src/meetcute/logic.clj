c05fb9f9f85c2c71a135b24617ed54488fdd5a03
Devon Zuegel <devonzuegel@gmail.com>
2024-01-22 14:06:46 -0500

    small tweaks

--- src/meetcute/logic.clj
+++ src/meetcute/logic.clj
@@ -185,7 +185,7 @@
       (let [cutie-first-name (first-name-bold new-todays-cutie-profile)
             email-config {:to        (:Email profile)
                           :from-name "MeetCute"
-                          :subject   (str " 🍊 Fresh cutie! Meet " (mc.util/get-field new-todays-cutie-profile "First name"))
+                          :subject   (str " Fresh cutie! 🍊 Meet " (mc.util/get-field new-todays-cutie-profile "First name"))
                           :body      (str "<div style='line-height: 1.6em; font-family: Roboto Mono, monospace !important; margin-top: 24px'>"
                                           (if cutie-first-name
                                             (str "Your cutie of the day is " cutie-first-name "! ")
